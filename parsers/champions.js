46665fe18c09c53fadb06ec5ab9e00b30408d5b5
Joshua Ting <jaedeeting@github.com>
2021-11-07 00:25:16 -0400

    V6 comps

--- parsers/champions.js
+++ parsers/champions.js
@@ -3,9 +3,11 @@ for (let rank = 1; rank <= 5; rank++) {
     let champIcons = document.getElementsByClassName("c" + rank);
     for (let i = 0; i < champIcons.length; i++) {
         let champ = champIcons[i].querySelector(".character-icon").getAttribute("alt");
+        let image = champIcons[i].querySelector(".character-icon").getAttribute("src");
         champs.push({
             name: champ,
-            cost: rank
+            cost: rank,
+            image: image
         })
     }
 }
